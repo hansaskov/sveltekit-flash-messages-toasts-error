354a2af887e4ce0c7f8160b6267f0b9d33be9eb1
hansaskov <hansaskov1999@gmail.com>
2023-07-15 23:54:04 +0200

    update message text

--- src/routes/private/+page.server.ts
+++ src/routes/private/+page.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from 'sveltekit-flash-message/server';
 import type { PageServerLoad } from './$types';
 
-const message = { type: 'error', message: 'You must be logged in to enter' } as const;
+const message = { type: 'error', message: 'Message from /private' } as const;
 
 export const load: PageServerLoad = async (event) => {
 	throw redirect(303, '/', message, event);
