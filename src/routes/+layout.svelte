3607d4753e4f10b61c69057658bbad64823b7179
hansaskov <hansaskov1999@gmail.com>
2023-07-15 23:47:33 +0200

    update navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,7 +33,7 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<a class="text-xl uppercase semibold" href="/"> Skeleton </a>
+				<a class="text-xl uppercase semibold" href="/"> Homepage </a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a class="btn btn-sm variant-ghost-surface" href="/private"> Private </a>
